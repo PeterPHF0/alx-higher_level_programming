ff8709a47d2299331eff421f18b337fbe3288c9a
root <peteriskander75@gmail.com>
2024-01-31 09:08:11 -0800

    new

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -44,7 +44,7 @@ class Square:
         return self.__size**2
     
     def my_print(self):
-        if self.__size == 0:
+        if self.size == 0:
             print()
         else:
             for row in range(0, self.__size):
