5ca3a18b076113414273eaaab40bb21769cc656f
root <peteriskander75@gmail.com>
2024-01-31 09:06:00 -0800

    new

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,35 +6,50 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size):
-        """Initialize a new square.
+    def __init__(self, size=0):
+        """Initialize a new Square.
 
         Args:
             size (int): The size of the new square.
         """
-        self.size = size
-
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """Get the size of the square.
+        Returns: size (int) : The size of the square.
+        """
+        return self.__size
 
     @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+    def set_size(self, value):
+        """Set the size of the square."""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
+        self.__size = size
+        
+        
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """Return the area of the square.
 
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size**2
+    
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            for row in range(0, self.__size):
+                for col in range(0, self.__size):
+                    print('#', end="")
+                print()
+            
+    
