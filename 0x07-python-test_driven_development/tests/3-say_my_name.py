939251942d08ef099ee2010c595d6a5dd2c25639
root <peteriskander75@gmail.com>
2024-02-20 09:35:38 -0800

    new

---
+++ 0x07-python-test_driven_development/tests/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a name-printing function"""
+
+def say_my_name(first_name, last_name=""):
+    """Print the name
+
+    Args:
+        first_name (str): first name
+        last_name (str, optional): last name. Defaults to "".
+
+    Raises:
+        TypeError: If either of first_name or last_name are not strings.
+    """
+    if type(first_name) != str:
+        raise TypeError('first_name must be a string')
+    elif type(last_name) != str:
+        raise TypeError('last_name must be a string')
+    
+    print(f"My name is {first_name} {last_name}")
