2c860a987038d9a0ca6bf856377be680c7d82457
root <peteriskander75@gmail.com>
2024-06-26 01:27:23 -0700

    py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 
 if __name__ == '__main__':
 
-    print("{} ".format(len(argv) - 1), end='')
-    if len(argv) - 1 == 0:
+    print("{} ".format(len(argv)), end='')
+    if len(argv) == 0:
         print("arguments.", end='')
-    elif len(argv) - 1 == 1:
+    elif len(argv) == 1:
         print("argument:")
     else:
         print("arguments:")
 
-    for i in range(len(argv) - 1):
+    for i in range(len(argv)):
         print("{}: {}".format(i+1, argv[i+1]))
